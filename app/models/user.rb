431d0cb42b3f637ca031e0d1d1e0e92fb2f27881
vagrant <vagrant@localhost.localdomain>
2020-08-12 13:54:02 +0000

    add model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
  has_many :followers, through: :reverse_of_relationships, source: :user
 
+ # dm機能
+ has_many :messages, dependent: :destroy
+ has_many :entries, dependent: :destroy
+
 
 
   def follow(other_user)
